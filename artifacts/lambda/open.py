2076db7eabf612611313735700c75c6a20a153c5
jonesdk1522 <kevin.d.jones.jr@gmail.com>
2021-01-13 13:20:59 -0500

    added servicenow post api

--- artifacts/lambda/open.py
+++ artifacts/lambda/open.py
@@ -1,10 +1,28 @@
 import json
 import uuid
+import requests
 
 def lambda_handler(event, context):
     
 
-    # Call ServiceNow API here
+    # Set the request parameters
+    url = 'https://https://dev36823.service-now.com/nav_to.do?uri=%2Fhome.do%3F'
+    user = 'admin'
+    pwd = 'VYfbTzwO4t8C'
+    
+    # Set proper headers
+    headers = {"Content-Type":"application/json","Accept":"application/json"}
+    
+    # Do the HTTP request
+    response = requests.post(url, auth=(user, pwd), headers=headers ,data='{"short_description":"Test"}')
+    
+    # Check for HTTP codes other than 200
+    if response.status_code != 201: 
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+        exit()
+    
+    # Decode the JSON response into a dictionary and use the data
+    print('Status:',response.status_code,'Headers:',response.headers,'Response:',response.json())
     
     return {
         "statusCode": 200,
