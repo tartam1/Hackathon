aa4fd3662e2addb878421617ad10335a52c4c8c4
jonesdk1522 <kevin.d.jones.jr@gmail.com>
2021-01-14 10:18:27 -0500

    restructuring open.py

--- artifacts/lambda/open.py
+++ artifacts/lambda/open.py
@@ -2,12 +2,12 @@ import json
 import uuid
 import requests
 
-# Botocore removed the vendored version of requests: This is a viable workaround.
+
 def lambda_handler(event, context):
     
 
     # Set the request parameters
-    url = 'https://https://dev36823.service-now.com/nav_to.do?uri=%2Fhome.do%3F'
+    url = 'https://dev36823.service-now.com/nav_to.do?uri=%2Fhome.do%3F'
     user = 'admin'
     pwd = 'VYfbTzwO4t8C'
     
