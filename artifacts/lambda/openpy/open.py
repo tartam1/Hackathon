3cf0fc0d07a1d68d58d3de5e80e36621fd4d3d4d
jonesdk1522 <kevin.d.jones.jr@gmail.com>
2021-01-14 14:24:03 -0500

    made working changes

--- artifacts/lambda/openpy/open.py
+++ artifacts/lambda/openpy/open.py
@@ -1,32 +1,29 @@
-import json
-import uuid
 import requests
 
-
-def lambda_handler(event, context):
-    
+def lambda_handler(event=None, context=None):
 
     # Set the request parameters
-    url = 'https://dev36823.service-now.com/api/now/table/incident'
+    url = 'https://dev36823.service-now.com/api/now/table/incident?sysparm_display_value=true&sysparm_exclude_reference_link=true&sysparm_fields=number%2Copened_by%2Cshort_description%2Cstate%2Cimpact'
+
+    # Username & Password
     user = 'admin'
     pwd = 'VYfbTzwO4t8C'
 
-        # Set proper headers
+    # Set proper headers
     headers = {"Content-Type":"application/json","Accept":"application/json"}
 
     # Do the HTTP request
-    response = requests.post(url, auth=(user, pwd), headers=headers ,data="{\"assigned_to\":\"\",\"active\":\"\",\"business_service\":\"\",\"opened_by\":\"\"}")
+    response = requests.post(url, auth=(user, pwd), headers=headers ,data="{\"short_description\":\"api test\"}")
 
     # Check for HTTP codes other than 200
-    if response.status_code != 200: 
+    if response.status_code != 201:
         print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
-        #exit()
+        exit()
 
     # Decode the JSON response into a dictionary and use the data
     data = response.json()
     print(data)
-    
-    return {
-        "statusCode": 200,
-        "ticketNumber": str(uuid.uuid1())
-    }
+
+    return data
+
+lambda_handler()
