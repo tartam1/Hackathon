f47a4d8458fe668cff091bc5016b662531382272
jonesdk1522 <kevin.d.jones.jr@gmail.com>
2021-01-14 12:44:00 -0500

    updated open.py to reflect working decoders

--- artifacts/lambda/openpy/open.py
+++ artifacts/lambda/openpy/open.py
@@ -7,23 +7,24 @@ def lambda_handler(event, context):
     
 
     # Set the request parameters
-    url = 'https://dev36823.service-now.com/nav_to.do?uri=%2Fhome.do%3F'
+    url = 'https://dev36823.service-now.com/api/now/table/incident'
     user = 'admin'
     pwd = 'VYfbTzwO4t8C'
-    
-    # Set proper headers
+
+        # Set proper headers
     headers = {"Content-Type":"application/json","Accept":"application/json"}
-    
+
     # Do the HTTP request
-    response = requests.post(url, auth=(user, pwd), headers=headers ,data='{"short_description":"Test"}')
-    
+    response = requests.post(url, auth=(user, pwd), headers=headers ,data="{\"assigned_to\":\"\",\"active\":\"\",\"business_service\":\"\",\"opened_by\":\"\"}")
+
     # Check for HTTP codes other than 200
-    if response.status_code != 201: 
+    if response.status_code != 200: 
         print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
-        exit()
-    
+        #exit()
+
     # Decode the JSON response into a dictionary and use the data
-    print('Status:',response.status_code,'Headers:',response.headers,'Response:',response.json())
+    data = response.json()
+    print(data)
     
     return {
         "statusCode": 200,
